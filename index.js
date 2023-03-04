7600d09a4abd7c9cfd7cc4ba7768e6ca0902388b
codeJSwing <imlogic20@gmail.com>
2023-03-04 17:35:59 +0900

    add product router

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import productRouter from "./routes/product.js"
 
-dotenv.config()
+
 
 const app = express()
+dotenv.config()
+
+app.use("/product", productRouter)
 
 const port = process.env.PORT || 9000
 app.listen(port, console.log("서버 started"))
