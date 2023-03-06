9166f635589e78347e7679bf7a208a9f2d731860
codeJSwing <imlogic20@gmail.com>
2023-03-06 12:03:52 +0900

    add order.js POST

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ import express from "express"
 import dotenv from "dotenv"
 import bodyParser from "body-parser"
 import productRouter from "./routes/product.js"
+import orderRouter from "./routes/order.js"
 import morgan from "morgan"
 
 const app = express()
 dotenv.config()
 
 app.use(morgan("dev"))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use("/product", productRouter)
+app.use("/order", orderRouter)
+
 
 const port = process.env.PORT || 9000
 app.listen(port, console.log("서버 started"))
--- routes/order.js
+++ routes/order.js
@@ -1,3 +1,24 @@
 import express from "express";
 const router = express.Router()
 
+router.get("/", (req, res) =>  {
+    res.json({
+        msg : "get all order"
+    })
+})
+
+router.post("/create", (req, res) => {
+    const newOrder = {
+        name : req.body.orderTitle,
+        price : req.body.orderPrice,
+        count : req.body.orderCount
+    }
+
+    res.json({
+        msg : "create new order",
+        orderInfo : newOrder
+    })
+})
+
+export default router
+
