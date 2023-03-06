a6588c8087c72333cf7535641fdbe85b97df135c
codeJSwing <imlogic20@gmail.com>
2023-03-06 12:09:51 +0900

    install mongoose & DB connection

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ import bodyParser from "body-parser"
 import productRouter from "./routes/product.js"
 import orderRouter from "./routes/order.js"
 import morgan from "morgan"
+import mongoose from "mongoose";
+
+const dbAddress = "mongodb+srv://imlogic20:cho1234@cluster0.h4zcfup.mongodb.net/?retryWrites=true&w=majority"
+mongoose
+    .connect(dbAddress)
+    .then(() => console.log("Mongo DB Connected"))
+    .catch(err => console.log(err.message))
 
 const app = express()
 dotenv.config()
