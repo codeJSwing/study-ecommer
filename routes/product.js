cab43bc587c04ad693f90662f05aa40ed04c438c
codeJSwing <imlogic20@gmail.com>
2023-03-10 10:12:26 +0900

    refactoring data(get, post)

--- routes/product.js
+++ routes/product.js
@@ -2,66 +2,63 @@ import express from "express"
 import productModel from "../models/product.js"
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    productModel
-        .find()
-        .then(products => {
-            res.json({
-                msg: `successful all products`,
-                count: products.length,
-                products: products
-            })
+router.get("/", async(req, res) => {
+    try {
+        const products = await productModel.find()
+        return res.json({
+            msg: "successful get products",
+            products: products.map(product => ({
+                name: product.name,
+                price: product.price,
+                id: product._id
+            }))
         })
-        .catch(err => {
-            res.status(404).json({
-                msg: err.message
-            })
+    } catch(err) {
+        res.status(404).json({
+            msg: err.message
         })
+    }
 })
 
-router.get("/:id", (req, res) => {
-   productModel
-       .findById(req.params.id)
-       .then(product => {
-           if(product == null){
-               res.json({
-                   msg: `no data`
-               })
-           }
-           res.json({
-               msg: `successful get product ${req.params.id}`,
-               product: product
-           })
-       })
-       .catch(err => {
-           res.json({
-               msg: err.message
-           })
-       })
+router.get("/:id", async (req, res) => {
+    const {id} = req.params
+    try {
+        const product = await productModel.findById(id)
+        if(!product){
+            return res.json({
+                msg: "no data"
+            })
+        }
+        res.json({
+            msg: "get data",
+            product
+        })
+    } catch (err) {
+        res.status(500).json({
+            msg: err.message
+        })
+    }
 })
 
-router.post("/", (req, res) => {
-    const newProduct = new productModel ({
-        name: req.body.productName,
-        price: req.body.productPrice,
-        desc: req.body.content
-    })
-    newProduct
-        .save()
-        .then(result => {
-            res.json({
-                msg: "create new product",
-                newProductInfo: {
-                    name: result.name,
-                    price: result.price
-                }
-            })
+router.post("/", async(req, res) => {
+    const {name, price, desc} = req.body
+
+    try{
+        const newProduct = new productModel({
+            name,
+            price,
+            desc
         })
-        .catch(err => {
-            res.status(404).json({
-                msg: err.message
-            })
+        const createProduct = await newProduct.save()
+        return res.json({
+            msg: "completed product",
+            product: createProduct
         })
+    } catch (err) {
+        res.status(500).json({
+            msg: err.message
+        })
+    }
 })
 
 router.put("/:id", (req, res) => {
